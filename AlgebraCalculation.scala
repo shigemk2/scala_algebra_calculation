01883ce3ecd4f1c23a711091733bc6322c69268e
shigemk2 <i.am.shige@gmail.com>
2015-06-14 22:21:14 +0900

    メソッド名をevalだった

--- AlgebraCalculation.scala
+++ AlgebraCalculation.scala
@@ -1,10 +1,15 @@
 sealed trait Algebra
 case class Add(n: Int*) extends Algebra
+case class Mul(n: Int*) extends Algebra
 
-def add(a: Add): Int =
+def eval(a: Add): Int =
   a.n.sum
 
-println(add(new Add(1,2)) == 3)
-println(add(new Add(2,3)) == 5)
-println(add(new Add(2,3,3)) == 8)
-println(add(new Add(5,-3)) == 2)
+def eval(a: Mul): Int =
+  a.n.product
+
+println(eval(new Add(1,2)) == 3)
+println(eval(new Add(2,3)) == 5)
+println(eval(new Add(2,3,3)) == 8)
+println(eval(new Add(5,-3)) == 2)
+println(eval(new Mul(3,4)) == 12)
