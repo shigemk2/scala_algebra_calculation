083a6c921d828c3802608154a96b6b7288621e25
shigemk2 <i.am.shige@gmail.com>
2015-06-14 20:17:08 +0900

    Haskell リスト化

--- Main.hs
+++ Main.hs
@@ -3,13 +3,14 @@ module Main where
 import Test.HUnit
 import System.IO
 
-data Add = Add Int Int deriving Show
+data Add = Add [Int] deriving Show
 
-eval (Add x y) = x + y
+eval (Add xs) = sum xs
 
 tests = TestList
-    [ "eval 1" ~: eval (Add 1 1) ~?= 1+1
-    , "eval 2" ~: eval (Add 2 3) ~?= 2+3
+    [ "eval 1" ~: eval (Add [1, 1]) ~?= 1+1
+    , "eval 2" ~: eval (Add [2, 3]) ~?= 2+3
+    , "eval 3" ~: eval (Add [2, 3, 3]) ~?= 2+3+3
     ]
 
 main = do
