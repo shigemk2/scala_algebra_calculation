1cfeb8113ec732cf2845a88a44c5260151fe3231
shigemk2 <i.am.shige@gmail.com>
2015-06-14 20:23:47 +0900

    Scala リスト化

--- AlgebraCalculation.scala
+++ AlgebraCalculation.scala
@@ -1,8 +1,10 @@
 sealed trait Algebra
-case class Add(x: Int, y: Int) extends Algebra
+case class Add(n: Int*) extends Algebra
 
-def add(x: Int, y: Int): Int =
-  x + y
+def add(n: Int*): Int =
+  n.sum
 
 println(add(1,1) == 2)
 println(add(2,3) == 5)
+println(add(2,3,3) == 8)
+println(add(2,3,3) == 7)
