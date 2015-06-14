d42da2c94b7113fe7614d0056b791ca785aa0d5b
shigemk2 <i.am.shige@gmail.com>
2015-06-14 20:35:56 +0900

    Haskell 掛け算

--- Main.hs
+++ Main.hs
@@ -3,14 +3,18 @@ module Main where
 import Test.HUnit
 import System.IO
 
-data Add = Add [Int] deriving Show
+data Expr = Add [Int]
+          | Mul [Int]
+          deriving Show
 
 eval (Add xs) = sum xs
+eval (Mul xs) = product xs
 
 tests = TestList
     [ "eval 1" ~: eval (Add [1, 1]) ~?= 1+1
     , "eval 2" ~: eval (Add [2, 3]) ~?= 2+3
     , "eval 3" ~: eval (Add [5, -3]) ~?= 5-3
+    , "eval 4" ~: eval (Mul [3, 4]) ~?= 3*4
     ]
 
 main = do
