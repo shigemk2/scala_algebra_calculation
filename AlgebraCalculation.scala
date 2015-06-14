fdea3ff5d8dd1646984f5181823d5a27ce7837f7
shigemk2 <i.am.shige@gmail.com>
2015-06-14 22:03:05 +0900

    足し算はこっち

--- AlgebraCalculation.scala
+++ AlgebraCalculation.scala
@@ -1,8 +1,8 @@
 sealed trait Algebra
-case class Add(n: Int*) extends Algebra
+case class Add(x: Int, y: Int) extends Algebra
 
 def add(a: Add): Int =
-  a.n.sum
+  a.x + a.y
 
 println(add(new Add(1,2)) == 3)
 println(add(new Add(2,3)) == 5)
