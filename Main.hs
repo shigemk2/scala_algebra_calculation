dfac93e4467cc7a462a55707ba84c0144f00ba7f
shigemk2 <i.am.shige@gmail.com>
2015-06-15 13:39:24 +0900

    Haskell 再帰型

--- Main.hs
+++ Main.hs
@@ -3,18 +3,22 @@ module Main where
 import Test.HUnit
 import System.IO
 
-data Expr = Add [Int]
-          | Mul [Int]
+data Expr = N Int
+          | Add [Expr]
+          | Mul [Expr]
           deriving Show
 
-eval (Add xs) = sum xs
-eval (Mul xs) = product xs
+eval (N   x ) = x
+eval (Add xs) = sum     [eval x | x <- xs]
+eval (Mul xs) = product [eval x | x <- xs]
 
 tests = TestList
-    [ "eval 1" ~: eval (Add [1, 1]) ~?= 1+1
-    , "eval 2" ~: eval (Add [2, 3]) ~?= 2+3
-    , "eval 3" ~: eval (Add [5, -3]) ~?= 5-3
-    , "eval 4" ~: eval (Mul [3, 4]) ~?= 3*4
+    [ "eval 1" ~: eval (Add[N 1,N  1 ]) ~?= 1+1
+    , "eval 2" ~: eval (Add[N 2,N  3 ]) ~?= 2+3
+    , "eval 3" ~: eval (Add[N 5,N(-3)]) ~?= 5-3
+    , "eval 4" ~: eval (Mul[N 3,N  4 ]) ~?= 3*4
+    , "eval 5" ~: eval (Add[N 1,Mul[N 2,N 3]]) ~?= 1+2*3
+    , "eval 6" ~: eval (Mul[Add[N 1,N 2],N 3]) ~?= (1+2)*3
     ]
 
 main = do
