bf973454d294ebc0d801c8d5e789c463d10ce1dd
shigemk2 <i.am.shige@gmail.com>
2015-06-16 00:09:56 +0900

    やっぱりダメっぽい

--- AlgebraCalculation.scala
+++ AlgebraCalculation.scala
@@ -1,20 +1,21 @@
-sealed trait Algebra
-case class N(n: Int) extends Algebra
-case class Add(n: N*) extends Algebra
-case class Mul(n: N*) extends Algebra
+sealed trait Algebra[+A]
+case class N[A](n: A) extends Algebra[A]
+case class Add[A](n: A*) extends Algebra[A]
+case class Mul[A](n: A*) extends Algebra[A]
 
-def eval(x: N): Int =
+def eval(x: N[Int]): Int =
   x.n
 
-def eval(xs: Add): Int =
+def eval(xs: Add[N[Int]]): Int =
   xs.n.map(x => eval(x)).sum
 
-def eval(xs: Mul): Int =
+def eval(xs: Mul[N[Int]]): Int =
   xs.n.map(x => eval(x)).product
 
+println(eval(Add(N(1),N(2))))
 println(eval(Add(N(1),N(2))) == 1+2)
 println(eval(Add(N(2),N(3))) == 2+3)
-println(eval(Add(N(2),N(3),N(3))) == 2*3*3)
+println(eval(Add(N(2),N(3),N(3))) == 2+3+3)
 println(eval(Add(N(5),N(-3))) == 5-3)
 println(eval(Mul(N(3),N(4))) == 3*4)
-println(eval(Mul(Add(N(1),N(2)),N(3))) == (1+2)*3)
+// println(eval(Mul(Add(N(1),N(2)),N(3))) == (1+2)*3)
