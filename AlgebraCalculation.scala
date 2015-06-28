061ece1f42a7e31f6d7a75b27ce24132d0d232bf
shigemk2 <i.am.shige@gmail.com>
2015-06-28 21:12:38 +0900

    再帰型 Haskellに近づけてみた

--- AlgebraCalculation.scala
+++ AlgebraCalculation.scala
@@ -1,15 +1,18 @@
-sealed trait Expr {
-  def eval(): Int = this match {
-    case N(x) => x
-    case Add(xs) => xs.map(x => x.eval).sum
-    case Mul(xs) => xs.map(x => x.eval).product
-  }
-}
+sealed trait Expr
 case class N(n: Int) extends Expr
 case class Add(n: List[Expr]) extends Expr
 case class Mul(n: List[Expr]) extends Expr
 
-println(Add(List(N(1),N(2))).eval == 1+2)
-println(Mul(List(N(3),N(4))).eval == 3*4)
-println(Mul(List(Add(List(N(1),N(2))),N(3))).eval == (1+2)*3)
-// println(eval(Mul(Add(N(1),N(2)),N(3))) == (1+2)*3)
+def eval(e: Expr): Int = e match {
+  case N(x) => x
+  case Add(xs) => xs.map(x => eval(x)).sum
+  case Mul(xs) => xs.map(x => eval(x)).product
+}
+
+println(eval(Add(List(N(1),N(2)))) == 1+2)
+println(eval(Add(List(N(2),N(3)))) == 2+3)
+println(eval(Add(List(N(5),N(-3)))) == 5-3)
+println(eval(Mul(List(N(3),N(4)))) == 3*4)
+println(eval(Add(List(N(1),Mul(List(N(2),N(3)))))) == 1+2*3)
+println(eval(Mul(List(Add(List(N(1),N(2))),N(3)))) == (1+2)*3)
+
