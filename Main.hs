ead4a797bd1dd9bcb6d0a49ac61b183fcba95458
shigemk2 <i.am.shige@gmail.com>
2015-07-01 00:33:00 +0900

    表示 Haskell

--- Main.hs
+++ Main.hs
@@ -12,6 +12,19 @@ eval (N   x ) = x
 eval (Add xs) = sum     [eval x | x <- xs]
 eval (Mul xs) = product [eval x | x <- xs]
 
+isneg (N n) | n < 0 = True
+isneg _             = False
+
+str (N x)     = show x
+str (Add [])  = ""
+str (Add [x]) = str x
+str (Add (x:y:zs))
+    | isneg y    = str x        ++ str (Add (y:zs))
+str (Add (x:xs)) = str x ++ "+" ++ str (Add xs)
+str (Mul [])     = ""
+str (Mul [x])    = str x
+str (Mul (x:xs)) = str x ++ "*" ++ str (Mul xs)
+
 tests = TestList
     [ "eval 1" ~: eval (Add[N 1,N  1 ]) ~?= 1+1
     , "eval 2" ~: eval (Add[N 2,N  3 ]) ~?= 2+3
@@ -19,6 +32,10 @@ tests = TestList
     , "eval 4" ~: eval (Mul[N 3,N  4 ]) ~?= 3*4
     , "eval 5" ~: eval (Add[N 1,Mul[N 2,N 3]]) ~?= 1+2*3
     , "eval 6" ~: eval (Mul[Add[N 1,N 2],N 3]) ~?= (1+2)*3
+    , "str 1" ~: str (Add[N 1,N  2 ,N  3 ])  ~?= "1+2+3"
+    , "str 2" ~: str (Add[N 1,N(-2),N(-3)])  ~?= "1-2-3"
+    , "str 3" ~: str (Mul[N 1,N  2 ,N  3 ])  ~?= "1*2*3"
+    , "str 4" ~: str (Add[N 1,Mul[N 2,N 3]]) ~?= "1+2*3"
     ]
 
 main = do
